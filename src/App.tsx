53c3ce69151ac5a287bb322279fd5eeed62970f7
TundraClimate <tundra.tundere@gmail.com>
2024-04-28 23:46:18 +0900

    feat: set application title

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,9 @@ import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>DiscordRPCEditor</h1>
+    </div>
+  );
 };
