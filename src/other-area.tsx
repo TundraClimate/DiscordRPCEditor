b61a0043fe359d0d4e312670edac63d72e0eb805
TundraClimate <tundra.tundere@gmail.com>
2024-04-30 18:03:20 +0900

    feat: enable link button

--- src/other-area.tsx
+++ src/other-area.tsx
@@ -3,11 +3,23 @@ import { Button } from "@suid/material";
 export const OtherArea = () => {
   return (
     <div class="other-area">
-      <Button color="inherit" size="large" variant="outlined">
-        &#8635;update activity
+      <Button
+        color="inherit"
+        size="large"
+        variant="outlined"
+        startIcon="&#8635;"
+      >
+        update activity
       </Button>
-      <Button color="primary" variant="outlined">
-        Develop Portal🔗
+      <Button
+        component="a"
+        href="https://discord.com/developers/applications"
+        target="_blank"
+        color="primary"
+        variant="outlined"
+        endIcon="🔗"
+      >
+        Develop Portal
       </Button>
     </div>
   );
